74f17916ed142ae3629ad4608751852b6eb665c9
Lucas Gabriel <lucasgabrielpd17@gmail.com>
2022-04-11 20:01:03 -0300

    Correção de teste

--- testes/1.test.js
+++ testes/1.test.js
@@ -2,9 +2,9 @@ const returnStr = require('../exercises/1')
 
 describe('Testa se, a função tem o retorno esperado', () => {
     it('Testa se, a função retorna "Olá Maria" ', () => {
-        expect(returnStr('Maria')).toBe("Olá Maria")
+        expect(returnStr('Maria')).toBe("Olá, Maria")
     }) 
     it('Testa se, a função retorna "Olá Leonardo', () => {
-        expect(returnStr('Leonardo')).toBe("Olá Leonardo")
+        expect(returnStr('Leonardo')).toBe("Olá, Leonardo")
     })
 })
